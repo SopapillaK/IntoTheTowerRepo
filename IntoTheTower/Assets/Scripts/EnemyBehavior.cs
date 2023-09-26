c28da9902b2c89e6c04ca8d25a76666ac9ded144
SopapillaKat <vellita2011@gmail.com>
2023-09-26 13:17:15 -0700

    enemy more

--- IntoTheTower/Assets/Scripts/EnemyBehavior.cs
+++ IntoTheTower/Assets/Scripts/EnemyBehavior.cs
@@ -1,9 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("PlayerObj").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+            Patrolling();
+        if (playerInSightRange && !playerInAttackRange)
+            ChasePlayer();
+        if (playerInSightRange && playerInAttackRange)
+            AttackPlayer();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+            SearchWalkPoint();
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+    }
+
+    private void SearchWalkPoint()
+    {
+        // calc random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+
+    }
+
+    private void AttackPlayer()
+    {
+
+    }
+
+    //Animation
     public Animator enemy;
     private int hp = 5; //Private value for hp
     public int enemyHP { get { return hp; }  //Get Doesn't need any functions, since we only need to check hp when changed
