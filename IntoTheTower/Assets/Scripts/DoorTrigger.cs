bc49bff733b57c6671fd7edf1999939ce3dd29b0
SopapillaKat <vellita2011@gmail.com>
2023-11-13 12:05:49 -0700

    text

--- IntoTheTower/Assets/Scripts/DoorTrigger.cs
+++ IntoTheTower/Assets/Scripts/DoorTrigger.cs
@@ -2,24 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
+using UnityEngine.UI;
 
 public class DoorTrigger : MonoBehaviour
 {
     public Animator animator;
     public AudioClip DoorClosed;
+    public GameObject BoxTrigger;
+    public bool BoxTriggerOn = true;
+    public GameObject firstText;
+    public GameObject secondText;
+    public GameObject thirdText;
 
     public void OnTriggerEnter(Collider collision)
     {
-        animator.SetTrigger("PlayerInTower");
-        AudioSource ac = GetComponent<AudioSource>();
-        ac.PlayOneShot(DoorClosed);
-
+        if (BoxTriggerOn)
+        {
+            animator.SetTrigger("PlayerInTower");
+            AudioSource ac = GetComponent<AudioSource>();
+            ac.PlayOneShot(DoorClosed);
+            firstText.SetActive(false);
+            secondText.SetActive(true);
+            Invoke("MainObjectiveTextOn", 6.0f);
+            Invoke("TurnTriggerBoolOff", 7.0f);
+            if (!BoxTriggerOn)
+            {
+                BoxTriggerOff();
+            }
+        }
     }
-    //public void CloseDoor()
-    //{
-    //    //Animator anim = Sword.GetComponent<Animator>();
-    //    animator.SetTrigger("PlayerInTower");
-    //    //AudioSource ac = GetComponent<AudioSource>();
-    //    //ac.PlayOneShot(SwordAttackSound);
-    //}
+    
+    public void MainObjectiveTextOn()
+    {
+        secondText.SetActive(false);
+        thirdText.SetActive(true);
+    }
+
+    public void BoxTriggerOff()
+    {
+        BoxTrigger.SetActive(false);
+    }
+
+    public void TurnTriggerBoolOff()
+    {
+        BoxTriggerOn = false;
+    }
 }
